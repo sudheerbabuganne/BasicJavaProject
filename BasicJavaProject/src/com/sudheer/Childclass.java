47d225c2d73bddde25496071ef548f548f67a20b
sudheer <sudheerbabuganne@gmail.com>
2018-02-09 21:32:52 +0530

    intial commit

---
+++ BasicJavaProject/src/com/sudheer/Childclass.java
@@ -0,0 +1,11 @@
+package com.sudheer;
+
+public class Childclass
+{
+	
+	public void display()
+	{
+		System.out.println("display method is calling");
+	}
+
+}
---
+++ BasicJavaProject/src/com/sudheer/Main.java
@@ -0,0 +1,17 @@
+package com.sudheer;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		Childclass obj1=new Childclass();
+		
+		System.out.println("main method is calling");
+		
+		obj1.display();
+
+	}
+
+}
